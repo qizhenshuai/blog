431eb158031d90491fb6456eeebaf2547aa0da39
qishuai <qi.shuai.baiso.com>
2020-10-02 22:29:39 +0800

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,6 +7,6 @@ git add -A
 git commit -m 'update'
 
 # 把上面的 <USERNAME> 换成你自己的 Github 用户名，<REPO> 换成仓库名，比如我这里就是：
-# git push -f git@github.com:qizhenshuai/blog.git master
+git push -f git@github.com:qizhenshuai/blog.git master
 
 cd -
